c52e53b335fc0dd803b578c60683ffd4f0d25ef4
Manuel Madrid <163127790+Shakeezy@users.noreply.github.com>
2025-04-29 19:47:09 +0200

    Delete hairdresser directory

--- hairdresser/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('', include('booking.urls')),
-]
